e4ed5d8db2b9a1a6bf3634d3d293c17677f2ed67
Krishnap2k4 <patel.krishna.t@gmail.com>
2025-07-22 23:22:37 +0530

    Final

---
+++ src/components/About/About.tsx
@@ -0,0 +1,111 @@
+import { Container } from "./styles";
+import Krishna from "../../assets/krishna.png";
+import python from "../../assets/python.svg"
+import java from "../../assets/java.svg"
+import wordpress from "../../assets/wordpress.svg";
+import shopify from "../../assets/shopify.svg";
+import htmlIcon from "../../assets/html-icon.svg";
+import cssIcon from "../../assets/css-icon.svg";
+import jsIcon from "../../assets/js-icon.svg";
+// import nodeIcon from "../../assets/node-icon.svg";
+import reactIcon from "../../assets/react-icon.svg";
+import typescriptIcon from "../../assets/typescript-icon.svg";
+import vueIcon from "../../assets/vue-icon.svg";
+import boostrapIcon from "../../assets/bootstrap-icon.svg";
+import ScrollAnimation from "react-animate-on-scroll";
+import devIcon from "../../assets/Dev.svg";
+
+export function About() {
+  return (
+    <Container id="about">
+      <div className="about-text">
+        <ScrollAnimation animateIn="fadeInLeft">
+          <h2>About me</h2>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.1 * 1000}>
+          <p>
+            Hi there! I'm Krishna, an enthusiastic Software Engineer skilled in web development, machine learning, and scalable software solutions
+          </p>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000} style={{ marginTop: "2rem", marginBottom: "2rem" }}>
+          <p>
+            I build responsive web applications, fine-tune ML models, and implement robust backend services.
+          </p>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.3 * 1000}>
+          <p>
+            Apart from my academics, I enjoy playing tabla and cricket, which helps me stay active, focused, and team-oriented.
+          </p>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={400}>
+          <div className="education">
+            <h3>Education:</h3>
+            <h4>B. Tech in Information Technology</h4>
+            <p>K.K.Wagh Institute of Engineering Education and Research, Nashik | May 2026</p>
+            <p>CGPA: 8.35/10.0</p>
+          </div>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={550}>
+          <div className="experience">
+             <h3>Internship:</h3><h4>Full Stack Developer</h4>
+            <p>DataCrushAnalytics | November 2023 – February 2024 </p>
+            <p>Nashik, India</p>
+          </div>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.4 * 1000}>
+          <h3>Here are my main skills:</h3>
+        </ScrollAnimation>
+        <div className="hard-skills">
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.10 * 1000}>
+              <img src={python} alt="python" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.11 * 1000}>
+              <img src={java} alt="java" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.12 * 1000}>
+              <img src={jsIcon} alt="JavaScript" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.13 * 1000}>
+              <img src={reactIcon} alt="React" />
+            </ScrollAnimation>
+          </div>
+         <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.13 * 1000}>
+              <img src={devIcon} alt="Devops" className="w-[100px] h-[100px] object-contain mx-auto" />
+            </ScrollAnimation>
+          </div>
+
+          
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.18 * 1000}>
+              <img src={htmlIcon} alt="Html" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
+              <img src={cssIcon} alt="Css" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.20 * 1000}>
+              <img src={boostrapIcon} alt="bootstrap" />
+            </ScrollAnimation>
+          </div>
+        </div>
+      </div>
+      <div className="about-image">
+        <ScrollAnimation animateIn="fadeInRight" delay={0.21 * 1000}>
+          <img src={Krishna} alt="Krishna" />
+        </ScrollAnimation>
+      </div>
+    </Container>
+  )
+}
